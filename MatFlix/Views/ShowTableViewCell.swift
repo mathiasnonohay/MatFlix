5d40862ae405aa9cd82df1db97b5864948e7350e
Mathias Nonohay <mathias12300@gmail.com>
2022-08-10 09:10:00 -0300

    Show table cell

--- MatFlix/Views/ShowTableViewCell.swift
+++ MatFlix/Views/ShowTableViewCell.swift
@@ -8,16 +8,15 @@
 import UIKit
 
 class ShowTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    static let identifier = "ShowTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError()
     }
-
+    
 }
